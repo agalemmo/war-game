4993ce962283bfe888fcf69c7a775b43268f8042
zgranoff <grano102@mail.chapman.edu>
2016-12-15 16:49:16 -0800

    Update WarGame.java
    Added some values and started the while loops.  Also deleted a duplicate printHand method.

--- WarGame.java
+++ WarGame.java
@@ -1,55 +1,76 @@
+import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class WarGame
 {
-    private static Deck playDeck = new Deck();
-    private static Deck playerDeck = new Deck();
-    private static Deck compDeck = new Deck();
+	private static Deck playDeck = new Deck();
+	private static Deck playerDeck = new Deck();
+	private static Deck compDeck = new Deck();
 
-    public static void main(String args[])
-    {
-        playDeck.makeDeck();
+	public static void main(String args[])
+	{
+		//stats
+		playDeck.makeDeck();
         splitHand();
 
-        for (int i = 0; i < 26; i++)
-        {
-            System.out.println("Round " + i + " of the game.");
+		int battles = 0; //number of battles
+		int wars = 0; //number of wars
+		int doubleWars = 0; //number of double wars
+		int count = 0; // number of battles
+		Boolean gameReset = false; //restarts the game within the while loop
 
-            for (int c = 0; c < 1; c++)
-            {
-                for (int player = 0; player < hands.length; player++)
-                {
-                    hands[player][0] = playerDeck.dealCard();
-                }
-            }
+		while(count < 1000)
+		{
+			count ++;
+			gameReset = false; //need to move this down?
 
-            for (int player = 0; player < hands.length; player++)
-            {
-                System.out.println("Player: " + player);
-                printHand(hands[player]);
+			for (int i = 0; i < 26; i++)
+			{
+				System.out.println("Round " + i + " of the game.");
 
-                int player1 = hands[0][0].getValue();
-                int player2 = hands[1][0].getValue();
+				for (int c = 0; c < 1; c++)
+				{
+					for (int player = 0; player < hands.length; player++)
+					{
+						hands[player][0] = playerDeck.dealCard();
+					}
+				}
 
-                if (player1 > player2)
-                {
-                    System.out.println("Player One wins!");
-                }
-                else if (player2 > player1)
-                {
-                    System.out.println("Player Two wins!");
-                }
-                else
-                {
-                    System.out.println("The war is tied.");
-                }
-            }
-        }
-    }
+				for (int player = 0; player < hands.length; player++)
+				{
+					System.out.println("Player: " + player);
+					printHand(hands[player]);
 
-    public static void splitHand()
+					int player1 = hands[0][0].getValue();
+					int player2 = hands[1][0].getValue();
+
+					if (player1 > player2)
+					{
+						System.out.println("Player One wins!");
+						battles = battles + 1;
+					}
+					else if (player2 > player1)
+					{
+						System.out.println("Player Two wins!");
+						battles = battles + 1;
+					}
+					else
+					{
+						System.out.println("The game is tied!  A war will decide the victor!");
+						wars = wars + 1;
+
+						while(gameReset == false)
+						{
+							// While loop for in the event of a War
+						}
+					}
+				} 
+			}
+		}
+	}
+
+	public static void splitHand()
     {
         for (int i = 0; i < playDeck.getSize(); i++)
         {
@@ -65,49 +86,13 @@ public class WarGame
         }
     }
 
-    public static void printHand()
-    {
-        for (int card = 0; card < hand.length; card++)
-        {
-            System.out.println(hand[card].toString());
-        }
-        System.out.println();
-    }
-    /**
-     * just does the averages, can probably put into the main method at some point
-     */
     public static void averages()
     {
-        avgBattles = totalBattles / 1000;
-        avgDoubleWars = totalDoubleWars / 1000;
-        avgWars = totalWars / 1000;
-    }
-    /**
-     * Takes a this value and then the comparative number, for the min/max variables, possibly unnecessary
-     * @param other
-     * @return -1 if other > this, 0 if other == this, 1 if this > other
-     */
-    public int compareTo(int other)
-    {
+        avgBattles = battles / 1000;
+        avgDoubleWars = doubleWars / 1000;
+        avgWars = wars / 1000;
     }
 
-    /**
-     * For 1000 games ...
-
-     Average number of battles per game: 321.2
-
-     Average number of wars per game: 24.3
-
-     Average number of double wars per game: 0.5
-
-     Max number of battles in a game: 1234
-
-     Min number of battles in a game: 12
-
-     Max number of wars in a game: 123
-
-     Min number of wars in a game: 1
-     */
     public static void printStats()
     {
         System.out.println("For 1000 games...");
@@ -118,14 +103,13 @@ public class WarGame
         System.out.println("Min number of battles in a game: " + minBattles);
         System.out.println("Max number of wars in a game: " + maxWars);
         System.out.println("Min number of wars in a game: " + minWars);
-    }
 
-    public static void printHand()
-    {
-        for (int card = 0; card < hand.length; card++)
-        {
-            System.out.println(hand[card].toString());
-        }
-        System.out.println();
-    }
+	public static void printHand()
+	{
+		for (int card = 0; card < hand.length; card++)
+		{
+			System.out.println(hand[card].toString());
+		}
+		System.out.println();
+	}
 }
